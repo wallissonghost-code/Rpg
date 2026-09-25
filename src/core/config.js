00985b7933aefc1f4d260ecbee551e839628fc01
ghostzinho <wallissonghost@gmail.com>
2026-09-25 17:06:04 -0300

    refactor: modularize game systems

---
+++ src/core/config.js
@@ -0,0 +1,8 @@
+export const CONFIG={
+ world:{width:12000,height:12000},
+ player:{radius:20,maxHp:100,speed:220,damage:20,attackRange:92,attackCooldown:.38,regenDelay:5,regenRate:1},
+ mob:{radius:19,maxHp:60,minSpeed:46,maxSpeed:64,aggroRange:155,maxChaseFromHome:300,disengageRange:360,attackRange:42,attackDamage:10,attackCooldown:1.15,regenDelay:4,regenRate:6,returnSpeedMultiplier:1.15,wanderRadius:130},
+ loot:{life:30,pickupRange:42,value:5},
+ deathBag:{openRange:45},
+ spawn:{encounters:85,maxGroup:6}
+};
